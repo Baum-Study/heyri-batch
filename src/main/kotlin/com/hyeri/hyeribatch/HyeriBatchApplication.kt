7c99cd0794fbe00c422cc8d858318c3ad883fcd8
cos850 <cos850@naver.com>
2024-11-11 22:10:28 +0900

    init

---
+++ src/main/kotlin/com/hyeri/hyeribatch/HyeriBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.hyeri.hyeribatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HyeriBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<HyeriBatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyeri/hyeribatch/HyeriBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyeri.hyeribatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyeriBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
